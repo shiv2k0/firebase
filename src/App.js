4a56818b55706b1bd7a21b5b482fa2545351a81b
Shivendra Singh <shivendrasingh@Shivendras-Mac-mini.local>
2023-03-06 15:25:22 +0530

    Uploading file on firebase Storage

--- src/App.js
+++ src/App.js
@@ -1,63 +1,37 @@
 import { useState } from "react";
-import { app, database } from "./firebaseConfig";
-import { collection, doc, updateDoc, deleteDoc } from "firebase/firestore";
+import { app, storage } from "./firebaseConfig";
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 
 function App() {
   const [data, setData] = useState({});
-  // const collectionRef = collection(database, "user");
-  // const handleChange = (event) => {
-  //   const newInput = { [event.target.name]: event.target.value };
-  //   setData({ ...data, ...newInput });
-  // };
-  
-  const updateData =() =>{
-     const docToUpdate = doc(database,"user","a9OR8Kqugv7RqtQmNV6A")
-     updateDoc(docToUpdate,{
-      // one or more than one field can be updated.
-      email: "shivavns00@gmail.com"
-     }).then(()=>{
-      alert("Updated Successfully")
-     }).catch(err=>{
-      alert(err.message)
-     })
-  }
-  const deleteData =()=>{
-    const docToDelete= doc(database,"user","a9OR8Kqugv7RqtQmNV6A")
-    deleteDoc(docToDelete).then(()=>{
-      alert("Deleted Successfully")
-    }).catch(err=>{
-      alert(err.message)
-    })
-  }
+  console.log(data)
   const handleSubmit = () => {
-    deleteData()
+    const storageRef = ref(storage, `images/${data.name}`);
+    const uploadTask = uploadBytesResumable(storageRef, data);
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        console.log("Upload is " + progress + "% done");
+        
+      },
+      (error) => {
+        console.log(error.message);
+      },
+      () => {
+        alert("File uploaded")
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          console.log("File available at ", downloadURL);
+        });
+      }
+    );
   };
   return (
     <div className="App">
+      <input type="file" onChange={(event) => setData(event.target.files[0])} />
+      <br />
 
-      {/* <label htmlFor="email">Email</label>
-      <input type="email" id="email" name="email" onChange={handleChange} />
-      <br />
-      <label htmlFor="password">Password</label>
-      <input
-        type="password"
-        id="password"
-        name="password"
-        onChange={handleChange}
-      />
-      <br />
-      <label htmlFor="comment">Comment</label>
-      <br />
-      <textarea
-        name="comment"
-        id="comment"
-        cols="30"
-        rows="5"
-        onChange={handleChange}
-      ></textarea>
-      <br /> */}
-
-      <button onClick={handleSubmit}>Delete Data</button>
+      <button onClick={handleSubmit}>Submit</button>
     </div>
   );
 }
--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,6 +1,7 @@
 
 import { initializeApp } from "firebase/app";
 import {getFirestore} from "firebase/firestore"
+import {getStorage} from 'firebase/storage'
 
 
 const firebaseConfig = {
@@ -14,3 +15,4 @@ const firebaseConfig = {
 
 export const app = initializeApp(firebaseConfig);
 export const database = getFirestore(app)
+export const storage = getStorage(app)
